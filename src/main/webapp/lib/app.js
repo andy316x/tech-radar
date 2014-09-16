678a0cda493f996c4ee359fb7941684c18735c8b
Andy Wilson <grandmaster_roc_raida@hotmail.com>
2014-09-16 22:57:07 +0100

    Add table of rows in CSV

--- src/main/webapp/lib/app.js
+++ src/main/webapp/lib/app.js
@@ -33,7 +33,6 @@ techRadarApp.directive('ngRadar', function () {
 			}, true);
 
 			$scope.$watch('selectedBlip', function (newVal, oldVal, scope) {
-				console.log(newVal);
 				if(newVal == null || typeof newVal == 'undefined') {
 					$scope.theRadar.unselectBlip(newVal);
 				} else {
@@ -62,6 +61,54 @@ techRadarApp.controller('RadarCtrl', function ($scope) {
 		$scope.selectedItem = null;
 	};
 
+	$scope.rows = [];
+	$scope.$watch('rows', function(newVal, oldVal, scope) {
+		$scope.arcMap = {};
+		$scope.quadrantMap = {};
+		$scope.radar.arcs = [];
+		$scope.radar.quadrants = [];
+		
+		for(var i = 0; i < newVal.length; i++) {
+			var row = newVal[i];
+			
+			var arc = $scope.arcMap[row.arcName];
+			if(arc == null || typeof arc == 'undefined') {
+				arc = {
+						id: row.arcName,
+						name: row.arcName,
+						r: arcWidths[$scope.radar.arcs.length],
+						color: arcColours[$scope.radar.arcs.length]
+				};
+				$scope.arcMap[row.arcName] = arc;
+				$scope.radar.arcs.push(arc);
+			}
+
+			var quadrant = $scope.quadrantMap[row.quadrantName];
+			if(quadrant == null || typeof quadrant == 'undefined') {
+				quadrant = {
+						id: row.quadrantName,
+						name: row.quadrantName,
+						color: quadrantColours[$scope.radar.quadrants.length],
+						items: []
+				};
+				$scope.quadrantMap[row.quadrantName] = quadrant;
+				$scope.radar.quadrants.push(quadrant);
+			}
+
+			quadrant.items.push({
+				id: i+1,
+				name: row.technologyName,
+				arc: row.arcName,
+				pc: {
+					r: row.radius,
+					t: Math.floor((Math.random() * 90) + 1)
+				},
+				movement: row.movement,
+				url: row.url
+			});
+		}
+	}, true);
+
 
 	$('#theFile').on('change', function(evt){
 
@@ -74,11 +121,8 @@ techRadarApp.controller('RadarCtrl', function ($scope) {
 			r.onload = function(e) { 
 
 				$scope.$apply(function(){
-					var arcMap = {};
-					var quadrantMap = {};
-					$scope.radar.arcs = [];
-					$scope.radar.quadrants = [];
-
+					$scope.rows = [];
+					
 					var contents = e.target.result;
 					var lines = contents.split('\n');
 					for(var i = 0; i < lines.length; i++) {
@@ -86,53 +130,16 @@ techRadarApp.controller('RadarCtrl', function ($scope) {
 							if(line.trim() != '') {
 								var cells = line.split(',');
 								if(cells.length == 8) {
-									var technologyName = cells[0].trim().substring(1, cells[0].trim().length-1);
-									var quadrantName = cells[1].trim().substring(1, cells[1].trim().length-1);
-									var arcName = cells[2].trim().substring(1, cells[2].trim().length-1);
-									var radius = parseInt(cells[3].trim().substring(1, cells[3].trim().length-1));
-									var theta = parseInt(cells[4].trim().substring(1, cells[4].trim().length-1));
-									var movement = cells[5].trim().substring(1, cells[5].trim().length-1);
-									var blipSize = parseInt(cells[6].trim().substring(1, cells[6].trim().length-1));
-									var url = cells[7].trim().substring(1, cells[7].trim().length-1);
-
-									var arc = arcMap[arcName];
-									if(arc == null || typeof arc == 'undefined') {
-										arc = {
-												id: arcName,
-												name: arcName,
-												r: arcWidths[$scope.radar.arcs.length],
-												color: arcColours[$scope.radar.arcs.length]
-										};
-										arcMap[arcName] = arc;
-										$scope.radar.arcs.push(arc);
-									}
-
-									var quadrant = quadrantMap[quadrantName];
-									if(quadrant == null || typeof quadrant == 'undefined') {
-										quadrant = {
-												id: quadrantName,
-												name: quadrantName,
-												color: quadrantColours[$scope.radar.quadrants.length],
-												items: []
-										};
-										quadrantMap[quadrantName] = quadrant;
-										console.log(quadrant);
-										console.log($scope.radar.quadrants);
-										$scope.radar.quadrants.push(quadrant);
-									}
-
-									quadrant.items.push({
-										id: i+1,
-										name: technologyName,
-										arc: arcName,
-										pc: {
-											r: radius,
-											t: Math.floor((Math.random() * 90) + 1)
-										},
-										movement: movement,
-										url: url
+									$scope.rows.push({
+										technologyName: cells[0].trim().substring(1, cells[0].trim().length-1),
+										quadrantName: cells[1].trim().substring(1, cells[1].trim().length-1),
+										arcName: cells[2].trim().substring(1, cells[2].trim().length-1),
+										radius: parseInt(cells[3].trim().substring(1, cells[3].trim().length-1)),
+										theta: parseInt(cells[4].trim().substring(1, cells[4].trim().length-1)),
+										movement: cells[5].trim().substring(1, cells[5].trim().length-1),
+										blipSize: parseInt(cells[6].trim().substring(1, cells[6].trim().length-1)),
+										url: cells[7].trim().substring(1, cells[7].trim().length-1)
 									});
-
 								} else {
 									console.log('invalid row');
 								}
