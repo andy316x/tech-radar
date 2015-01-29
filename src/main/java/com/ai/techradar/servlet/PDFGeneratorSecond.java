e9851bd782fef0322e3a4af868cceacc7241f35e
Andy Wilson <grandmaster_roc_raida@hotmail.com>
2015-01-29 01:21:25 +0000

    Fixed the PDF export

--- src/main/java/com/ai/techradar/servlet/PDFGeneratorSecond.java
+++ src/main/java/com/ai/techradar/servlet/PDFGeneratorSecond.java
@@ -45,27 +45,24 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 	private final Font headerFont = new Font(Font.HELVETICA, 14, Font.BOLD);
 	private final Font subheaderFont = new Font(Font.HELVETICA, 11, Font.BOLD);
 	private final Font textFont = new Font(Font.HELVETICA, 8, Font.NORMAL);
-	private RadarService service = (RadarService)SpringStarter.getContext().getBean("RadarService");
+	private final RadarService service = (RadarService)SpringStarter.getContext().getBean("RadarService");
 
-	private final static float optimumWidth = 650; // 750;
+	private final static float optimumWidth = 650;
 	private final static float width = 650;
 	private final static float scaleFactor = width / optimumWidth;
-	private final int height = 650; // 750;
+	private final int height = 650;
 
-	private static float a1 = scaleFactor * 210;// 240; 210
-	private static float a2 = scaleFactor * 210;// 240;
-	private static float a3 = scaleFactor * 115;// 135; 115
-	private static float a4 = scaleFactor * 115;// 135;
-	private static float a5 = scaleFactor * 115;// 135;
+	private static float a1 = scaleFactor * 150;
+	private static float a2 = scaleFactor * 125;
+	private static float a3 = scaleFactor * 75;
+	private static float a4 = scaleFactor * 50;
+	private static float a5 = scaleFactor * 50;
 
 	private static final float[] ARC_WIDTHS = new float[] { a1, a2, a3, a4, a5 };
-	// must add up to int width
 
-	private static final String[] ARC_COLOURS = new String[] { "rgb(223,223,223)", "rgb(166,167,169)", "rgb(190,191,193)", "rgb(209,209,209)", "rgb(223,223,223)" };
+	private static final Color[] ARC_COLOURS = new Color[] { new Color(223,223,223), new Color(166,167,169), new Color(190,191,193), new Color(209,209,209), new Color(223,223,223) };
 
 	private final Map<String, List<RadarTechnologyTO>> map = new LinkedHashMap<String, List<RadarTechnologyTO>>();
-
-	private final RadarTO r = service.getRadarById(new Long(1));
 
 	ByteArrayOutputStream bOut = new ByteArrayOutputStream();
 
@@ -74,24 +71,21 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 	PdfWriter writer;
 
-	private static final String[] QUADRANT_COLOURS = new String[] { "#3DB5BE", "#83AD78",
-		"#E88744", "#8D2145" };
+	private static final String[] QUADRANT_COLOURS = new String[] { "#428BCA", "#D9534F", "#5CB85C", "#f0AD4E" };
 	int colourCount = 0;
 
-	public void makeImageRadar(Document document, final Long id) {
+	public void makeImageRadar(final Document document, final RadarTO radar) {
 
 		try {
 
 			final float w = scaleFactor * optimumWidth;
 			final float h = height;
 
-			final RadarTO radar = service.getRadarById(id);
-
 			final Map<String, Arc> arcMap = new LinkedHashMap<String, Arc>();
 			for (final MaturityTO maturity : radar.getMaturities()) {
 				final Arc arc = new Arc((int) ARC_WIDTHS[arcMap.size()], maturity.getName(),
-							ARC_COLOURS[arcMap.size()]);
-					arcMap.put(maturity.getName(), arc);
+						ARC_COLOURS[arcMap.size()]);
+				arcMap.put(maturity.getName(), arc);
 			}
 
 			float totalArc = 0;
@@ -118,15 +112,15 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 			final Map<String, Quadrant> quadrantMap = new HashMap<String, Quadrant>();
 			for (final TechGroupingTO techGrouping : radar.getTechGroupings()) {
-				Quadrant quadrant = new Quadrant(techGrouping.getName(),
-							QUADRANT_COLOURS[quadrantMap.size()]);
+				final Quadrant quadrant = new Quadrant(techGrouping.getName(),
+						QUADRANT_COLOURS[quadrantMap.size()]);
 				quadrant.setStartTheta(quadrantMap.size() * 90);
-					quadrantMap.put(techGrouping.getName(), quadrant);
+				quadrantMap.put(techGrouping.getName(), quadrant);
 			}
 
 			// add technologies to quad
 
-			for (final RadarTechnologyTO tech : r.getTechnologies()) {
+			for (final RadarTechnologyTO tech : radar.getTechnologies()) {
 				String techGroupingName = tech.getTechGrouping();
 				List<RadarTechnologyTO> techGrouping = map.get(techGroupingName);
 				if (techGrouping == null) {
@@ -145,56 +139,51 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 				// write techs
 
 				headerFont.setColor(Color.decode(quadrant.getColour()));
-				String chapTitle = quadrant.getName().toUpperCase();
-				Chunk chunk = new Chunk(chapTitle, headerFont);
+				final String chapTitle = quadrant.getName().toUpperCase();
+				final Chunk chunk = new Chunk(chapTitle, headerFont);
 				chunk.setLocalDestination(chapTitle);
-				Chapter chapter = new Chapter(new Paragraph(chunk), chapNumber);
+				final Chapter chapter = new Chapter(new Paragraph(chunk), chapNumber);
 				chapNumber++;
 
 				document.add(chapter);
 
 				// draw quadrant
-				PdfContentByte cb = writer.getDirectContent();
-				PdfTemplate tp = cb.createTemplate(optimumWidth, height);
-				Graphics2D g2d = tp.createGraphics(optimumWidth, height, new DefaultFontMapper());
+				final PdfContentByte cb = writer.getDirectContent();
+				final PdfTemplate tp = cb.createTemplate(optimumWidth, height);
+				final Graphics2D g2d = tp.createGraphics(optimumWidth, height, new DefaultFontMapper());
 
-				final int r1 = (int) a1;
-				final int r2 = (int) r1 + (int) a2; // cumulative of arc widths
-				final int r3 = (int) r2 + (int) a3;
-				final int r4 = (int) r3 + (int) a4;
-				final int r5 = (int) r4 + (int) a5;
-				
-				g2d.setColor(new Color(223, 223, 223));
-				g2d.fillArc(r5 / 2 - r1 / 2, r5 / 2 - r1 / 2, r5, r5, 90, 90);
+				// Loop around the arcs in reverse and draw them
+				final List<Arc> arcs = new ArrayList<Arc>();
+				for(final Arc arc : arcMap.values()) {
+					arcs.add(arc);
+				}
+				final Arc outerArc = arcs.get(arcs.size()-1);
+				for(int i = arcs.size(); i > 0; i--) {
+					final Arc arc = arcs.get(i-1);
+					g2d.setColor(arc.getColour());
+					g2d.fillArc(outerArc.getOuterRadius() - arc.getOuterRadius(), outerArc.getOuterRadius() - arc.getOuterRadius(), arc.getOuterRadius()*2, arc.getOuterRadius()*2, 90, 90);
+				}
 
-				g2d.setColor(new Color(209, 209, 209));
-				g2d.fillArc(r5 / 2 - r4 / 2, r5 / 2 - r4 / 2, r4, r4, 90, 90);
 
-				g2d.setColor(new Color(190, 191, 193));
-				g2d.fillArc(r5 / 2 - r3 / 2, r5 / 2 - r3 / 2, r3, r3, 90, 90);
-
-				g2d.setColor(new Color(166, 167, 169));
-				g2d.fillArc(r5 / 2 - r2 / 2, r5 / 2 - r2 / 2, r2, r2, 90, 90);
-
-				g2d.setColor(new Color(223, 223, 223));
-				g2d.fillArc(r5 / 2 - r1 / 2, r5 / 2 - r1 / 2, r1, r1, 90, 90);
-				
-				
 				g2d.setFont(new java.awt.Font("Verdana", Font.NORMAL, 8));
 				g2d.setColor(Color.decode(quadrant.getColour()));
 
-				int offset = (int) (scaleFactor * 38);
-
-				g2d.drawString("PHASE OUT", scaleFactor * 330, offset);
-				g2d.drawString("MAINTAIN", scaleFactor * 330, offset + (scaleFactor * 60));
-				g2d.drawString("INVEST", scaleFactor * 330, offset + (scaleFactor * 125));
-				g2d.drawString("ASSESS", scaleFactor * 330, offset + (scaleFactor * 240));
-				g2d.drawString("WATCH", scaleFactor * 330, offset + (scaleFactor * 240));
+				int cumaltive = 0;
+				for(int i = arcs.size(); i > 0; i--) {
+					final Arc arc = arcs.get(i-1);
+					
+					final int next = arc.getOuterRadius() - arc.getInnerRadius();
+					final int half = next / 2;
+					
+					g2d.drawString(arc.getName().toUpperCase(), scaleFactor * 330, cumaltive + half);
+					
+					cumaltive += next;
+				}
 
 				final int axisWidth = 8;
 				g2d.setPaint(new Color(1, 1, 1, 0.5f));
-				g2d.fillRect(r4 / 2 - axisWidth / 2, 0, axisWidth, r4);
-				g2d.fillRect(0, r4 / 2 - axisWidth / 2, r4, axisWidth);
+				g2d.fillRect(outerArc.getOuterRadius() - axisWidth / 2, 0, axisWidth, outerArc.getOuterRadius() * 2);
+				g2d.fillRect(0, outerArc.getOuterRadius() - axisWidth / 2, outerArc.getOuterRadius() * 2, axisWidth);
 
 				g2d.setColor(Color.decode(quadrant.getColour()));
 
@@ -216,20 +205,20 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 				}
 
 				// separate into sub headings
-				List<RadarTechnologyTO> techList = quadrant.getItems();
+				final List<RadarTechnologyTO> techList = quadrant.getItems();
 
-				HashMap<String, Paragraph> mapArcNameToPDFParagraph = new HashMap<String, Paragraph>();
-				ArrayList<String> arcNames = new ArrayList<String>();
+				final Map<String, Paragraph> mapArcNameToPDFParagraph = new HashMap<String, Paragraph>();
+				final List<String> arcNames = new ArrayList<String>();
 
 				techIndexRadar = techIndex + 1;
-				for (RadarTechnologyTO tech : techList) {
-					String arcName = tech.getMaturity();
+				for (final RadarTechnologyTO tech : techList) {
+					final String arcName = tech.getMaturity();
 					if (mapArcNameToPDFParagraph.containsKey(arcName)) {
-						Paragraph currentParagraph = mapArcNameToPDFParagraph.get(arcName);
+						final Paragraph currentParagraph = mapArcNameToPDFParagraph.get(arcName);
 						currentParagraph.setFont(textFont);
 						currentParagraph.add(++techIndex + ". " + tech.getTechnology() + "\n");
 					} else {
-						Paragraph techParagraph = new Paragraph();
+						final Paragraph techParagraph = new Paragraph();
 						techParagraph.setFont(textFont);
 						mapArcNameToPDFParagraph.put(arcName, techParagraph);
 						techParagraph.add(++techIndex + ". " + tech.getTechnology() + "\n");
@@ -245,46 +234,29 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 						for (int l = 0; l < techs.size(); l++) {
 							final RadarTechnologyTO item = techs.get(l);
 
-							// what does this do?
-
-							// Quadrant techQuadrant =
-							// quadrantMap.get(item.getQuadrantName());
-							// if (techQuadrant == null) {
-							// techQuadrant = new
-							// Quadrant(item.getQuadrantName(),
-							// QUADRANT_COLOURS[quadrantMap.size()]);
-							// techQuadrant.setStartTheta(quadrantMap.size() *
-							// 90);
-							// quadrantMap.put(item.getQuadrantName(),
-							// techQuadrant);
-							// }
-
 							final Arc arc = arcMap.get(item.getMaturity());
-							final float r = (((arc.getOuterRadius() - arc.getInnerRadius()) / ((rails
-									.size()) + 1)) * (k + 1)) + arc.getInnerRadius();
+							final float r = (((arc.getOuterRadius() - arc.getInnerRadius()) / ((rails.size()) + 1)) * (k + 1)) + arc.getInnerRadius();
 
 							final float segmentWidth = 90 / (techs.size() + 1);
-							final double x = (w / 2) + r
-									* Math.cos(rad(segmentWidth * (l + 1) + 180));
-							final double y = (h / 2) + r
-									* Math.sin(rad(segmentWidth * (l + 1) + 180));
+							final double x = (w / 2) + r * Math.cos(rad(segmentWidth * (l + 1) + 180));
+							final double y = (h / 2) + r * Math.sin(rad(segmentWidth * (l + 1) + 180));
 
 							// blip draw
 
-							int blipSize = (int) scaleFactor * 14;
-							int oneCorrection = (int) (scaleFactor * 2);
-							double tempWhiteOutline = scaleFactor * 4;
+							final int blipSize = (int) scaleFactor * 14;
+							final int oneCorrection = (int) (scaleFactor * 2);
+							final double tempWhiteOutline = scaleFactor * 4;
 
-							int whiteOutline = (int) Math.ceil(tempWhiteOutline);
-							float fontSizeUpto99 = scaleFactor * 8;
+							final int whiteOutline = (int) Math.ceil(tempWhiteOutline);
+							final float fontSizeUpto99 = scaleFactor * 8;
 
-							float fontSizeAbove99 = scaleFactor * 6;
-							int lowNumberPositionCorrection = (int) scaleFactor * 2;
-							int highNumberPositionCorrection = (int) scaleFactor * 1;
+							final float fontSizeAbove99 = scaleFactor * 6;
+							final int lowNumberPositionCorrection = (int) scaleFactor * 2;
+							final int highNumberPositionCorrection = (int) scaleFactor * 1;
 
 
 
-							g2d.setColor(Color.WHITE);
+							g2d.setColor(Color.decode(quadrant.getColour()));
 							g2d.fillArc((int) x - ((blipSize + whiteOutline) / 2) , (int) y - 2,
 									blipSize + whiteOutline, blipSize + whiteOutline, 0, 360);
 							g2d.setColor(Color.decode(quadrant.getColour()));
@@ -326,17 +298,15 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 
 
-				for (String arcName : arcNames) {
-					//for (String arcName : orderedArcNames) {
-					System.out.println("ARC NAME: " + arcName);
+				for (final String arcName : arcNames) {
 					subheaderFont.setColor(Color.decode(quadrant.getColour()));
-					Paragraph subHeading = new Paragraph(arcName.toUpperCase(), subheaderFont);
+					final Paragraph subHeading = new Paragraph(arcName.toUpperCase(), subheaderFont);
 					document.add(subHeading);
 					document.add(mapArcNameToPDFParagraph.get(arcName));
 				}
 
 			}
-		} catch (DocumentException e) {
+		} catch (final DocumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -348,30 +318,30 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 	public void makeContentsPage(Document document) {
 
-		Font spacesFont = new Font(Font.HELVETICA, 8, Font.NORMAL);
+		final Font spacesFont = new Font(Font.HELVETICA, 8, Font.NORMAL);
 		spacesFont.setColor(Color.WHITE);
 
 		headerFont.setColor(Color.black);
 		textFont.setColor(Color.BLACK);
 
-		Font contentsHeaderFont = new Font(Font.HELVETICA, 10, Font.NORMAL);
+		final Font contentsHeaderFont = new Font(Font.HELVETICA, 10, Font.NORMAL);
 
-		String title3 = "CONTENTS";
-		Anchor anchor3 = new Anchor(title3, contentsHeaderFont);
+		final String title3 = "CONTENTS";
+		final Anchor anchor3 = new Anchor(title3, contentsHeaderFont);
 		anchor3.setName(title3);
-		Chapter chapter3 = new Chapter(new Paragraph(anchor3), 0);
+		final Chapter chapter3 = new Chapter(new Paragraph(anchor3), 0);
 
-		for (Integer pgNum : contentsPage.keySet()) {
+		for (final Integer pgNum : contentsPage.keySet()) {
 			String chapterTitle = contentsPage.get(pgNum);
-			int openBracket = chapterTitle.indexOf("[");
-			int endChapterNumber = chapterTitle.indexOf(".");
-			String chapterNumber = chapterTitle.substring(openBracket + 1, endChapterNumber);
-			int comma = chapterTitle.indexOf(",");
-			int closeBracket = chapterTitle.indexOf("]");
+			final int openBracket = chapterTitle.indexOf("[");
+			final int endChapterNumber = chapterTitle.indexOf(".");
+			final String chapterNumber = chapterTitle.substring(openBracket + 1, endChapterNumber);
+			final int comma = chapterTitle.indexOf(",");
+			final int closeBracket = chapterTitle.indexOf("]");
 			chapterTitle = chapterTitle.substring(comma + 1, closeBracket);
 
-			Chunk numberTitleChunk = new Chunk(chapterNumber + "      " + chapterTitle, textFont);
-			Chunk pageNumChunk = new Chunk("      " + pgNum + "\n", textFont);
+			final Chunk numberTitleChunk = new Chunk(chapterNumber + "      " + chapterTitle, textFont);
+			final Chunk pageNumChunk = new Chunk("      " + pgNum + "\n", textFont);
 			numberTitleChunk.setLocalGoto(chapterTitle.trim());
 			chapter3.add(numberTitleChunk);
 			chapter3.add(pageNumChunk);
@@ -379,19 +349,21 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 		}
 		try {
 			document.add(chapter3);
-		} catch (DocumentException e) {
+		} catch (final DocumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public void MakePDF(HttpServletResponse response, final Long id) {
+	public void makePDF(final HttpServletResponse response, final Long id) {
 		try {
+
+			final RadarTO radar = service.getRadarById(id);
 
 			// arcs
 
 			final Map<String, Arc> arcMap = new LinkedHashMap<String, Arc>();
-			for (final MaturityTO maturity : r.getMaturities()) {
+			for (final MaturityTO maturity : radar.getMaturities()) {
 				Arc arc = arcMap.get(maturity.getName());
 				if (arc == null) {
 					arc = new Arc((int) ARC_WIDTHS[arcMap.size()], maturity.getName(),
@@ -401,7 +373,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 			}
 
-			Document document = new Document(PageSize.A4);
+			final Document document = new Document(PageSize.A4);
 			bOut = new ByteArrayOutputStream();
 
 			// PdfWriter
@@ -411,7 +383,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 
 			document.open();
 
-			makeImageRadar(document, id);
+			makeImageRadar(document, radar);
 			makeContentsPage(document);
 
 			// Close and flush
@@ -424,20 +396,20 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			// now the first page
 			// Use a PdfStamper to then write this PDF but this time to the
 			// response output stream
-			PdfReader reader = new PdfReader(bOut.toByteArray());
-			int n = reader.getNumberOfPages();
+			final PdfReader reader = new PdfReader(bOut.toByteArray());
+			final int n = reader.getNumberOfPages();
 
-			List<Integer> pages = new ArrayList<Integer>();
+			final List<Integer> pages = new ArrayList<Integer>();
 			pages.add(n);
 			for (int i = 1; i < n; i++) {
 				pages.add(i);
 			}
 			reader.selectPages(pages);
-			PdfStamper stamper = new PdfStamper(reader, response.getOutputStream());
+			final PdfStamper stamper = new PdfStamper(reader, response.getOutputStream());
 
 			stamper.close();
 			bOut.close();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			System.out.printf("ERROR: ", e.getMessage());
 		}
 	}
@@ -451,13 +423,13 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 	private static class Arc {
 		private final int radius;
 		private final String name;
-		private final String colour;
+		private final Color colour;
 		private int innerRadius;
 		private int outerRadius;
 		private int rails;
 		private int index;
 
-		public Arc(int radius, String name, String colour) {
+		public Arc(final int radius, final String name, final Color colour) {
 			this.radius = radius;
 			this.name = name;
 			this.colour = colour;
@@ -471,7 +443,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			return name;
 		}
 
-		public String getColour() {
+		public Color getColour() {
 			return colour;
 		}
 
@@ -479,7 +451,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			return innerRadius;
 		}
 
-		public void setInnerRadius(int innerRadius) {
+		public void setInnerRadius(final int innerRadius) {
 			this.innerRadius = innerRadius;
 		}
 
@@ -487,7 +459,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			return outerRadius;
 		}
 
-		public void setOuterRadius(int outerRadius) {
+		public void setOuterRadius(final int outerRadius) {
 			this.outerRadius = outerRadius;
 		}
 
@@ -495,7 +467,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			return rails;
 		}
 
-		public void setRails(int rails) {
+		public void setRails(final int rails) {
 			this.rails = rails;
 		}
 
@@ -503,7 +475,7 @@ public class PDFGeneratorSecond extends PdfPageEventHelper {
 			return index;
 		}
 
-		public void setIndex(int index) {
+		public void setIndex(final int index) {
 			this.index = index;
 		}
 	};
--- src/main/java/com/ai/techradar/servlet/SvgConvertServlet.java
+++ src/main/java/com/ai/techradar/servlet/SvgConvertServlet.java
@@ -6,25 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 //import com.lowagie.text.Paragraph;
-import com.lowagie.text.Font;
 //import com.lowagie.text.Section;
 
 public class SvgConvertServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -6114901392531397724L;
-
-	//private RadarService service = new RadarServiceImpl();
-
-	/** The fonts for the title. */
-	public static final Font[] FONT = new Font[4];
-	static {
-		FONT[0] = new Font(Font.HELVETICA, 24);
-		// FONT[1] = new Font(FontFamily.HELVETICA, 18);
-		// FONT[2] = new Font(FontFamily.HELVETICA, 14);
-		FONT[3] = new Font(Font.HELVETICA, 12, Font.BOLD);
-	}
 
 	@Override
 	public void doPost(final HttpServletRequest request, final HttpServletResponse response)
@@ -32,14 +19,14 @@ public class SvgConvertServlet extends HttpServlet {
 		try {
 			response.setContentType("application/pdf");
 			response.addHeader("Content-Disposition", "attachment;filename=\"export.pdf\"");
-			
+
 			final Long id = Long.parseLong(request.getParameter("id"));
 
-			PDFGeneratorSecond test = new PDFGeneratorSecond();
-			test.MakePDF(response, id);
+			final PDFGeneratorSecond test = new PDFGeneratorSecond();
+			test.makePDF(response, id);
 
 			response.flushBuffer();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO log properly
 			e.printStackTrace();
 		}
