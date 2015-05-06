fc69d4db9f6bb7fd1b8c578092189468fc73cd8d
Hywel Bennett <hyweldt@gmail.com>
2015-05-06 09:02:10 +0100

    Moves app out of lib directory

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -5,7 +5,7 @@ var inject = require('gulp-inject');
  
 gulp.task('index', function () {
 	return gulp.src('./raw/index.html')
-	  .pipe(inject(gulp.src('./lib/controllers/*.js', {read: false}), {name: 'controllers', addPrefix: 'radar'}))
-  	  .pipe(inject(gulp.src('./lib/directives/*.js', {read: false}), {name: 'directives', addPrefix: 'radar'}))
+	  .pipe(inject(gulp.src('./app/controllers/*.js', {read: false}), {name: 'controllers', addPrefix: 'radar'}))
+  	  .pipe(inject(gulp.src('./app/directives/*.js', {read: false}), {name: 'directives', addPrefix: 'radar'}))
 	  .pipe(gulp.dest('.'));
 });
